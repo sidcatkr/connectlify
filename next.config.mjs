5ef6fbed61b5b44c1ea137de81b4791015573c42
sidcatkr <82746617+sidcatkr@users.noreply.github.com>
2025-05-02 17:57:03 +0000

    test

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,17 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'v0.blob.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
